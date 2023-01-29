0b2e1708e7fbdfe6f98ade6ce182aec21c1a392f
f026 <f0260001@gmail.com>
2023-01-29 21:34:57 +0300

    release v 1.4

--- main.java
+++ main.java
@@ -83,19 +83,49 @@ class MainWindow
     {
         Settings sett = new Settings();
         MessageBox MB = new MessageBox();
-        JFrame window = new JFrame("Main window");
+        JFrame window = new JFrame("калькулятор");
         JButton exit  = new JButton("выход");
         JButton settings = new JButton("настройки");
+        JButton nine = new JButton("9");
+        JButton eight = new JButton("8");
+        JButton seven = new JButton("7");
+        JButton six = new JButton("6");
+        JButton five = new JButton("5");
+        JButton four = new JButton("4");
+        JButton three = new JButton("3");
+        JButton two = new JButton("2");
+        JButton one = new JButton("1");
+        JButton zero = new JButton("0");
+        JTextField textField = new JTextField();
+        textField.setLocation(80,10);
+        textField.setSize(240,30);
         exit.setLocation(310,340);
         exit.setSize(80,20);
         settings.setLocation(10,340);
         settings.setSize(120,20);
+        nine.setLocation(80,50);
+        nine.setSize(80,60);
+        eight.setLocation(160,50);
+        eight.setSize(80,60);
+        seven.setLocation(240,50);
+        seven.setSize(80,60);
+        six.setLocation(80,110);
+        six.setSize(80,60);
+        five.setLocation(160,110);
+        five.setSize(80,60);
+        four.setLocation(240,110);
+        four.setSize(80,60);
+        three.setLocation(80,170);
+        three.setSize(80,60);
+        two.setLocation(160,170);
+        two.setSize(80,60);
+        one.setLocation(240,170);
+        one.setSize(80,60);
         settings.addActionListener(new ActionListener()
          {
             public void actionPerformed(ActionEvent e) 
             {
                 window.dispose();
-
                 sett.window();
             }
        });
@@ -106,9 +136,18 @@ class MainWindow
             MB.window();
         }
         });
-
+        window.add(textField);
         window.add(exit);
         window.add(settings);
+        window.add(nine);
+        window.add(eight);
+        window.add(seven);
+        window.add(six);
+        window.add(five);
+        window.add(four);
+        window.add(three);
+        window.add(two);
+        window.add(one);
         window.setSize(400, 400);
         window.setLocationRelativeTo(null);
         window.setLayout(null);
